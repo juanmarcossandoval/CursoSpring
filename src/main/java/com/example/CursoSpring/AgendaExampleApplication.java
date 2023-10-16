6bcc9b36d503568f4eb98abab6d33f41edd232bc
juanmarcossandoval <juanmarcos.sandoval@gmail.com>
2023-10-16 10:59:37 -0300

    creation of proyect

---
+++ src/main/java/com/example/CursoSpring/AgendaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.CursoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CursoSpring/AgendaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CursoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
